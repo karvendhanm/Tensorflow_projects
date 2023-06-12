b389593a535e499058fa0a212fdbdc6e27e8d113
Karvendhan M <karvsmech@gmail.com>
2023-06-12 06:02:52 +0530

    normalized term frequency using TfidfTransformer

--- advanced_NLP_tensorflow2/count_vectorization_tfidf.py
+++ advanced_NLP_tensorflow2/count_vectorization_tfidf.py
@@ -1,6 +1,9 @@
-from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
+# Count Vectorizer
 corpus = [
     'I like fruits. Fruits like bananas',
     'I love bananas but eat an apple',
@@ -16,9 +19,18 @@ X.toarray()
 # to compare similarity between documents
 cosine_similarity(X)
 
-query = vectorizer.transform(['apple and bananas'])
+# when an unknown token is encountered for transform operation
+# after the fit has happened in CountVectorizer, CountVectorizer
+# simply ignores it.
+query = vectorizer.transform(['Mini apple and bananas'])
+# the word "Mini" has been ignored.
 query.toarray()
 
-# checking the similarity of the documents wit thw given key words.
 cosine_similarity(X, query)
+
+# tfidf vectorizer and transformer.
+# use_idf = False gives us normalized term frequency.
+tfidf_transform = TfidfTransformer(use_idf=False)
+X_tf = tfidf_transform.fit_transform(X)
+X_tf.toarray()
 
