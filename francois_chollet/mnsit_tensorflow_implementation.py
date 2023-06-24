71b67dfd52bbf722465bdae139ced29c070320d9
Karvendhan M <karvsmech@gmail.com>
2023-06-24 16:23:13 +0530

    implementing mnist handwritten label classification with tensorflow without using keras

--- francois_chollet/mnsit_tensorflow_implementation.py
+++ francois_chollet/mnsit_tensorflow_implementation.py
@@ -1,5 +1,9 @@
 import math
+import numpy as np
 import tensorflow as tf
+
+from tensorflow.keras import optimizers
+from tensorflow.keras.datasets import mnist
 
 # creating a naive dense class using tensorflow, without the aid of keras.
 class NaiveDense:
@@ -55,12 +59,56 @@ class BatchGenerator:
         self.index += self.batch_size
         return images, labels
 
+learning_rate = 1e-3
+def update_weights(gradients, weights):
+    for g, w in zip(gradients, weights):
+        w.assign_sub(g * learning_rate)
+
+optimizer  =optimizers.SGD(learning_rate=1e-3)
+def update_weights_v1(gradients, weights):
+    optimizer.apply_gradients(zip(gradients, weights))
+
+def one_training_step(model, images_batch, labels_batch):
+    with tf.GradientTape() as tape:
+        predictions = model(images_batch)
+        per_sample_losses = tf.keras.losses.sparse_categorical_crossentropy(labels_batch, predictions)
+        average_loss = tf.reduce_mean(per_sample_losses)
+    gradients = tape.gradient(average_loss, model.weights)
+    update_weights(gradients, model.weights)
+    return average_loss
+
+def fit(model, images, labels, epochs, batch_size=128):
+    for epoch_counter in range(epochs):
+        print(f'Epoch {epoch_counter}')
+        batch_generator = BatchGenerator(images, labels)
+        for batch_counter in range(batch_generator.num_batches):
+            images_batch, labels_batch = batch_generator.next()
+            loss = one_training_step(model, images_batch, labels_batch)
+            if batch_counter % 100 == 0:
+                print(f'loss at batch {batch_counter}: {loss:.2f}')
 
 model = NaiveSequential([
     NaiveDense(input_size=28*28, output_size=512, activation=tf.nn.relu),
     NaiveDense(input_size=512, output_size=10, activation=tf.nn.softmax),
 ])
 assert len(model.weights) == 4
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape(60000, 28 * 28)
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape(10000, 28 * 28)
+test_images = test_images.astype('float32') / 255
+
+fit(model, train_images, train_labels, epochs=10, batch_size=128)
+
+# evaluating the model
+predictions = model(test_images)
+predictions = predictions.numpy()
+predicted_labels = np.argmax(predictions, axis=1)
+matches = predicted_labels == test_labels
+print(f'accuracy: {matches.mean():.2f}')
+
 
 
 
