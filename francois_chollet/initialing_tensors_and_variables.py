aef0c588d1f48d36dfe64e87ac0126727e182d69
Karvendhan M <karvsmech@gmail.com>
2023-06-28 07:39:19 +0530

    tensor operations

---
+++ francois_chollet/initialing_tensors_and_variables.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+
+tensor_zeros = tf.zeros(shape= (128, 128))
+print(tensor_zeros)
+
+tensor_ones = tf.ones(shape= (128, 128))
+
+x = tf.random.normal(shape= (3, 1), mean= 0., stddev=1.)
+print(x)
+
+x = tf.random.uniform(shape= (3, 1), minval= 0., maxval=1.)
+print(x)
+
+x = tf.ones(shape= (2, 2))
+x[0, 0] = 0.
+
+# mutable state tensor.
+initial_value = tf.random.uniform((3, 1))
+v = tf.Variable(initial_value=initial_value)
+print(v)
+
+v.assign(tf.ones((3, 1)))
+
+# assigning value to the subset of the tensor
+v[0, 0].assign(3.)
+
+v.assign_add(tf.ones((3, 1)))
+v.assign_sub(tf.ones((3, 1)))
+
+print(v)
+
+# doing math in tensorflow:
+a = tf.ones((2, 2))
+print(a)
+
+b = tf.square(a)
+print(b)
+
+c = tf.sqrt(a)
+print(c)
+
+d = b + c
+print(d)
+
+e = tf.matmul(a, d) # matrix multiplication(matmul) is a dot product
+print(e)
+
+e *= d
+print(e)  # this is element-wise multiplication
+
