e6e65756b0dd279302051cc5acc6ac323e7e3856
Karvendhan M <karvsmech@gmail.com>
2023-07-02 08:03:48 +0530

    keras dense layer - creating state just in time

---
+++ francois_chollet/creating_the_state_just_in_time.py
@@ -0,0 +1,28 @@
+# Keras Dense layer implementation - creating state just in time
+
+import tensorflow as tf
+from tensorflow import keras
+
+class SimpleDense(keras.layers.Layer):
+    def __init__(self, units, activation=None):
+        super().__init__()
+        self.units = units
+        self.activation = activation
+
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        self.W = self.add_weight(shape=(input_dim, self.units),
+                                 initializer='random_normal')
+        self.b = self.add_weight(shape=(self.units,),
+                                 initializer='zeros')
+
+    def call(self, inputs):
+        y = tf.matmul(inputs, self.W) + self.b
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
+
+my_dense = SimpleDense(units=32, activation=tf.nn.relu)
+input_tensor = tf.ones(shape=(2, 784))
+output_tensor = my_dense(input_tensor)
+print(output_tensor.shape)
