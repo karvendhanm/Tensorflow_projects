0babf08ac955bd42a3d0e68679937135e808729e
Karvendhan M <karvsmech@gmail.com>
2023-07-07 04:23:33 +0530

    rectifying an error

--- francois_chollet/binary_classification_imdb.py
+++ francois_chollet/binary_classification_imdb.py
@@ -65,28 +65,28 @@ history = model.fit(x=partial_X_train, y=partial_y_train, batch_size=512,
 
 # history object has a member history containing data about everything that happened
 # during training
-# history_dict = history.history
-# loss_values = history_dict['loss']
-# val_loss_values = history_dict['val_loss']
-# epochs = range(1, len(loss_values)+1)
-# plt.plot(epochs, loss_values, "bo", label='Training loss')
-# plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.xlabel('Epochs')
-# plt.ylabel('Loss')
-# plt.legend()
-# plt.show()
-#
-# plt.clf() # clears the figure
-# acc = history_dict['accuracy']
-# val_acc = history_dict['val_accuracy']
-# plt.plot(epochs, acc, 'bo', label='Training acc')
-# plt.plot(epochs, val_acc, 'b', label='Validation acc')
-# plt.title('Training and Validation accuracy')
-# plt.xlabel('Epochs')
-# plt.ylabel('Accuracy')
-# plt.legend()
-# plt.show()
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values)+1)
+plt.plot(epochs, loss_values, "bo", label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf() # clears the figure
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
 
 # the model seems to overfit (over optimization on the training data) after 4th epoch.
 # lets train the same model again with only 4 epochs
@@ -97,6 +97,10 @@ model.compile(optimizer='rmsprop',
 model.fit(x=partial_X_train, y=partial_y_train, batch_size=512, epochs=4)
 results = model.evaluate(X_test, y_test)
 results
+
+# prediction
+model.predict(X_test)
+
 
 
 
