1afb6a52cb4755e186a924d3d0ff7692006431ed
Karvendhan M <karvsmech@gmail.com>
2023-07-07 11:11:08 +0530

    plots for loss and accuracy for training and validation data

--- francois_chollet/multiclass_classification.py
+++ francois_chollet/multiclass_classification.py
@@ -1,9 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import reuters
 from tensorflow.keras.utils import to_categorical
+
+matplotlib.use('agg')
 
 (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=100000)
 
@@ -55,6 +59,47 @@ y_test = categorical_encoding(test_labels)
 # inbuild keras function for categorical encoding
 # y_train_ = to_categorical(train_labels)
 # y_test_ = to_categorical(test_labels)
+
+# creating validation set from training data
+X_val = X_train[:1000]
+partial_X_train = X_train[1000:]
+y_val = y_train[:1000]
+partial_y_train = y_train[1000:]
+
+# model architecture
+model = keras.Sequential([
+    layers.Dense(64, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(46, activation='softmax')  # this is a multi-class classification with 46 labels
+])
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(x=partial_X_train, y=partial_y_train, batch_size=512, epochs=20, validation_data=(X_val, y_val))
+
+# loss and accuracy plots (comparing training and validation sets)
+history_dict = history.history
+training_loss = history_dict['loss']
+validation_loss = history_dict['val_loss']
+training_accuracy = history_dict['accuracy']
+validation_accuracy = history_dict['val_accuracy']
+epochs = range(1, len(training_loss) + 1)
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 8))
+axes = axes.flatten()
+axes[0].plot(epochs, training_loss, 'bo', label='Training loss')
+axes[0].plot(epochs, validation_loss, 'b', label='Validation loss')
+axes[0].set_xlabel('Epochs')
+axes[0].set_ylabel('Loss')
+axes[0].legend()
+axes[1].plot(epochs, training_accuracy, 'bo', label='Training acc')
+axes[1].plot(epochs, validation_accuracy, 'b', label='Validation acc')
+axes[1].set_xlabel('Epochs')
+axes[1].set_ylabel('Accuracy')
+axes[1].legend()
+plt.savefig('./data/images/reuters.png')
 
 
 
