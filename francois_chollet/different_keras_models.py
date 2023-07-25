5c35e1e38deb748120b570e264d635a96431c852
Karvendhan M <karvsmech@gmail.com>
2023-07-25 08:14:02 +0530

    3 difference ways to build keras models

---
+++ francois_chollet/different_keras_models.py
@@ -0,0 +1,7 @@
+"""
+There are 3 different ways to building keras models.
+    1) The Sequential model - mostly used by Novice users
+    2) Functional API - mostly used by engineers
+    3) model subclassing - mostly used by researchers
+"""
+
