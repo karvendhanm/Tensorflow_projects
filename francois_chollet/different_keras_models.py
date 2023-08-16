a7c2f8841ad37640978752cf2196138204334e02
karvendhanm <karvsmech@gmail.com>
2023-08-16 08:29:24 +0530

    training multi-input, multi-output model using keras api

--- francois_chollet/different_keras_models.py
+++ francois_chollet/different_keras_models.py
@@ -5,10 +5,12 @@ There are 3 different ways to building keras models.
     3) model subclassing - mostly used by researchers
 """
 
-# the Sequential model/sequential API example
+import numpy as np
 
 from tensorflow import keras
 from tensorflow.keras import layers
+
+# the Sequential model/sequential API example
 
 model = keras.Sequential([
     layers.Dense(64, activation='relu', name='first_layer'),
@@ -74,21 +76,46 @@ model.summary()
 # 1) priority score of the ticket between 0 and 1
 # 2) the department that should handle the ticket
 
-vocabulary_size = 10000
-num_tags = 100
-num_departments = 4
+vocabulary_size = 10000  # size of the vocabulary of the text_input
+num_tags = 100  # assumed to be one-hot encoded
+num_department = 4
 
 title = keras.Input(shape=(vocabulary_size,), name='title')
 text_body = keras.Input(shape=(vocabulary_size,), name='text_body')
 tags = keras.Input(shape=(num_tags,), name='tags')
 
-features = layers.Concatenate()(title, text_body, tags)
+features = layers.Concatenate()([title, text_body, tags])
 features = layers.Dense(64, activation='relu')(features)
 
 priority = layers.Dense(1, activation='sigmoid', name='priority')(features)
-department = layers.Dense(num_departments, activation='softmax')(features)
+department = layers.Dense(num_department, activation='softmax', name='department')(features)
+model = keras.Model(inputs=[title, text_body, tags], outputs=[priority, department])
 
-model = keras.Model(inputs=(title, text_body, tags), outputs=(priority, department))
+
+model.weights
+model.summary()
+
+# training multi-input, multi-output keras model using functional-api
+num_samples = 1280
+
+title_data = np.random.randint(0, 2, size=(num_samples, vocabulary_size))
+text_body_data = np.random.randint(0, 2, size=(num_samples, vocabulary_size))
+tags_data = np.random.randint(0, 2, size=(num_samples, num_tags))
+
+priority_data = np.random.random(size=(num_samples, 1))
+department_data = np.random.randint(0, 2, size=(num_samples, num_department))
+
+model.compile(optimizer='rmsprop',
+              loss=['mean_squared_error', 'categorical_crossentropy'],
+              metrics=[['mean_absolute_error'], ['accuracy']])
+
+model.fit(x=[title_data, text_body_data, tags_data],
+          y=[priority_data, department_data],
+          epochs=1)
+model.evaluate([title_data, text_body_data, tags_data], [priority_data, department_data])
+priority_preds, department_data = model.predict([title_data, text_body_data, tags_data])
+
+
 
 
 
