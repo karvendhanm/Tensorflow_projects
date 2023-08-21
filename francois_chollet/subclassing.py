6c32a6038310f5147c2a7028164f64252b4cd374
karvendhanm <karvsmech@gmail.com>
2023-08-21 08:31:02 +0530

    subclassing a layer

--- francois_chollet/subclassing.py
+++ francois_chollet/subclassing.py
@@ -4,20 +4,48 @@ from tensorflow import keras
 
 
 # subclassing a layer
-class SimpleDense(keras.layers.Layer):
-
-    def __init__(self, units, activation):
+class DenseLayer(keras.layers.Layer):
+    def __init__(self, units, activation=None):
         """
 
-        :param units: number of outputs from a layer.
+        :param units:
         :param activation:
         """
         super().__init__()
+        self.W = None
+        self.b = None
         self.units = units
         self.activation = activation
 
+
     def build(self, input_shape):
+        """
+
+        :param input_dim:
+        :return:
+        """
         input_dim = input_shape[-1]
+        self.W = self.add_weight(shape=(input_dim, self.units), initializer='random_normal')
+        self.b = self.add_weight(shape=(self.units, ), initializer='zeros')
+
+
+    def call(self, inputs):
+        """
+
+        :param inputs:
+        :return:
+        """
+        y = tf.matmul(inputs, self.W) + self.b
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
+
+
+denselayer = DenseLayer(32, tf.nn.relu)
+input_tensor = tf.ones(shape=(2, 784))
+output_tensor = denselayer(input_tensor)
+print(output_tensor.shape)
+
 
 
 
