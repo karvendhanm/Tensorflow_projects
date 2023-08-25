48b6a123b31e53a232a17d30ea2c4802cc8ca748
karvendhanm <karvsmech@gmail.com>
2023-08-25 07:39:21 +0530

    custom metrics

--- francois_chollet/7_3_using_builtin_training_and_evaluation_loops.py
+++ francois_chollet/7_3_using_builtin_training_and_evaluation_loops.py
@@ -30,6 +30,44 @@ model.fit(x=train_images, y=train_labels, epochs=3, validation_data=(val_images,
 test_metrics = model.evaluate(x=test_images, y=test_labels)
 predictions = model.predict(test_images)
 
+# 7_3_1 writing your own metrics
+class RootMeanSquaredError(keras.metrics.Metric):
+
+    def __init__(self, name='rmse', **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.mse_sum = self.add_weight(name='mse_sum', initializer='zeros')
+        self.total_samples = self.add_weight(name='total_samples', initializer='zeros', dtype='int32')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.one_hot(y_true, depth=tf.shape(y_pred)[1])
+        mse = tf.reduce_sum(tf.square(y_true - y_pred))
+        self.mse_sum.assign_add(mse)
+        num_samples = tf.shape(y_pred)[0]
+        self.total_samples.assign_add(num_samples)
+
+    def result(self):
+        return tf.sqrt(self.mse_sum/tf.cast(self.total_samples, tf.float32))
+
+    def reset_state(self):
+        self.mse_sum.assign(0.)
+        self.total_samples.assign(0)
+
+
+model = get_mnist_model()
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy', RootMeanSquaredError()])
+model.fit(x=train_images, y=train_labels, epochs=3, validation_data=(val_images, val_labels))
+test_metrics = model.evaluate(x=test_images, y=test_labels)
+predictions = model.predict(test_images)
+
+
+
+
+
+
+
+
+
+
 
 
 
