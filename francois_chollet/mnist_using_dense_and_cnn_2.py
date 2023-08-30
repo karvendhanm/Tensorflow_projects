71c3ad85d9ab5c82f731115c5e0c5e9a51ea0b04
karvendhanm <karvsmech@gmail.com>
2023-08-30 08:30:29 +0530

    small cnn model using mnist dataset

--- francois_chollet/mnist_using_dense_and_cnn_2.py
+++ francois_chollet/mnist_using_dense_and_cnn_2.py
@@ -3,6 +3,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
 
 inputs = keras.Input(shape=(28, 28, 1), name='input_layer')  # shape - (image height, image width, image channels)
 x = layers.Conv2D(filters=32, kernel_size=3, activation='relu', name='first_conv_layer')(inputs)
@@ -17,3 +18,13 @@ model = keras.Model(inputs=inputs, outputs=outputs)
 keras.utils.plot_model(model, './model_structure/mnist_cnn.png', show_shapes=True)
 model.summary()
 
+# importing mnist data
+(train_data, train_labels), (test_data, test_labels) = mnist.load_data()
+train_data = train_data.reshape(60000, 28, 28, 1).astype('float32') / 255
+test_data = test_data.reshape(10000, 28, 28, 1).astype('float32') / 255
+
+model.compile(optimizer='rmsprop',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy']
+              )
+model.fit(x=train_data, y=train_labels, epochs=30, batch_size=2048)
