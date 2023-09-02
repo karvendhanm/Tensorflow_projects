798ae5df6832bd183bfca9fe65a3b37865c7788f
karvendhanm <karvsmech@gmail.com>
2023-09-02 14:39:35 +0530

    tensorflow dataset objects

---
+++ francois_chollet/tensorflow_dataset_objects.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+
+random_numbers = np.random.normal(size=(1000, 16))
+dataset = tf.data.Dataset.from_tensor_slices(random_numbers, name='my_data')
+
+for i, element in enumerate(dataset):
+    print(element.shape)
+    if i >= 2:
+        break
+
+batched_dataset = dataset.batch(32)
+for i, element in enumerate(batched_dataset):
+    print(element.shape)
+    if i >= 2:
+        break
+
+reshaped_dataset = dataset.map(lambda x: tf.reshape(x, (4, 4)))
+for i, element in enumerate(reshaped_dataset):
+    print(element.shape)
+    if i >= 2:
+        break
+
+
+
