26f39498fc223f5f63b7f94928ec130979294574
karvendhanm <karvsmech@gmail.com>
2023-09-02 09:02:44 +0530

    computer vision kaggle dataset (cats vs dogs) - building train, validation and test set

---
+++ francois_chollet/CV_cats_vs_dogs.py
@@ -0,0 +1,33 @@
+# Computer vision - Cats vs. Dogs
+import os
+import pathlib
+import shutil
+
+old_dir = pathlib.Path('./data/train')
+new_base_dir = pathlib.Path('./data/cats_vs_dogs_small')
+
+
+def make_subset(subset_name, start_index, stop_index):
+    """
+
+    :param subset_name:
+    :param start_index:
+    :param stop_index:
+    :return:
+    """
+    for category in ('cat', 'dog'):
+
+        if not os.path.exists(new_base_dir / subset_name / category):
+            pathlib.Path.mkdir(new_base_dir / subset_name / category, parents=True, exist_ok=True)
+
+        fnames = [f'{category}.{i}.jpg' for i in range(start_index, stop_index)]
+
+        for fname in fnames:
+            shutil.copy(old_dir / fname, new_base_dir / subset_name / category / fname)
+
+    return None
+
+
+make_subset(subset_name='train', start_index=0, stop_index=1000)
+make_subset(subset_name='validation', start_index=1000, stop_index=1500)
+make_subset(subset_name='test', start_index=1500, stop_index=2500)
