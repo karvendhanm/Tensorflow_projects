25557682b4deecc4249819f0e3adbc264ed7ad84
karvendhanm <karvsmech@gmail.com>
2023-10-09 18:42:12 +0530

    reading in segmentation map

--- francois_chollet/image_segmentation_.py
+++ francois_chollet/image_segmentation_.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import os
+import random
 
 from tensorflow.keras.utils import load_img, img_to_array
 from tensorflow.keras.utils import image_dataset_from_directory
 
 from tensorflow.python.ops.numpy_ops import np_config
-
 np_config.enable_numpy_behavior()
 
 input_dir = './data/images/'
@@ -38,6 +39,7 @@ plt.imshow(load_img(input_img_paths[9]))
 plt.axis('off')
 plt.savefig('./plots/image_segmentation_load_img.png')
 
+
 # # plot the target image
 # plt.imshow(load_img(target_paths[9], color_mode='grayscale'))
 # plt.axis('off')
@@ -54,5 +56,49 @@ def display_target(target_array):
     plt.axis('off')
     plt.savefig('./plots/target_segmentation_map_load_img.png')
 
+
 img = img_to_array(load_img(target_paths[9], color_mode='grayscale'))
 display_target(img)
+
+
+def path_to_input_image(input_path):
+    """
+
+    :param input_path:
+    :return:
+    """
+    return img_to_array(load_img(input_path, target_size=img_size))
+
+
+def path_to_target(target_path):
+    """
+
+    :return:
+    """
+    target_array = img_to_array(load_img(target_path, target_size=img_size, color_mode='grayscale'))
+    target_array = target_array.astype('uint8') - 1
+    return target_array
+
+
+img_size = (200, 200)
+num_imgs = len(input_img_paths)
+input_imgs = np.zeros(shape=(num_imgs,) + img_size + (3,), dtype='float32')
+targets = np.zeros(shape=(num_imgs,) + img_size + (1,), dtype='uint8')
+for i in range(num_imgs):
+    input_imgs[i] = path_to_input_image(input_img_paths[i])
+    targets[i] = path_to_target(target_paths[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
