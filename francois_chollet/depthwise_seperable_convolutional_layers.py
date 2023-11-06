2702f73143d1c904e7a088f4712ee810ba340961
karvendhanm <karvsmech@gmail.com>
2023-11-06 08:13:27 +0530

    implementing depthwise convolutional neural networks - where we assume deptwise channels are independent

---
+++ francois_chollet/depthwise_seperable_convolutional_layers.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.datasets import mnist
+
+# Implement depthwise seperable convolutional neural networks.
