98b856a02004eb76882d9aba83d83ffc40b59e6f
karvendhanm <karvsmech@gmail.com>
2024-04-30 09:46:51 +0530

    deep learning for text

---
+++ francois_chollet/deep_learning_for_text.py
@@ -0,0 +1,43 @@
+import string
+
+
+class Vectorizer(object):
+    def __init__(self):
+        self.inverse_dictionary = None
+
+    def standardize(self, text):
+        text = text.lower()
+        return "".join(char for char in text if char not in string.punctuation)
+
+    def tokenize(self, text):
+        text = self.standardize(text)
+        return text.split()
+
+    def make_vocabulary(self, dataset):
+        self.vocabulary = {"": 0, "[UNK]": 1}
+        for text in dataset:
+            tokens = self.tokenize(text)
+            for token in tokens:
+                if token not in self.vocabulary:
+                    self.vocabulary[token] = len(self.vocabulary)
+        self.inverse_dictionary = {v: k for k, v in self.vocabulary.items()}
+
+    def encode(self, text):
+        tokens = self.tokenize(text)
+        return [self.vocabulary.get(token, 1) for token in tokens]
+
+    def decode(self, int_sequence):
+        return " ".join([self.inverse_dictionary.get(i, '[UNK') for i in int_sequence])
+
+dataset = [
+    "I write, erase, rewrite",
+    "Erase again, and then",
+    "A poppy blooms.",
+]
+
+obj = Vectorizer()
+obj.make_vocabulary(dataset)
+
+test_sentence = "I write, rewrite, and still write again"
+encoded_sequence = obj.encode(test_sentence)
+obj.decode(encoded_sequence)
